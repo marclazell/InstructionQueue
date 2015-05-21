45e58cfa1507f46aefd1a24bdf348c383749df04
marclazell <lazell@live.co.uk>
2015-05-21 07:51:12 +0100

    .gitignore file Initial test class

---
+++ src/com/tp/test/InstructionQueueTest.java
@@ -0,0 +1,15 @@
+package com.tp.test;
+
+/**
+ * Created by dev on 21/05/15.
+ */
+public class InstructionQueueTest {
+
+    // create queue with length -1 - fail
+    // create queue with length 10 - pass
+    // create queue with length 100000 - pass
+    // create queue with no length - pass
+    // create queue with length "10" - fail
+
+
+}
