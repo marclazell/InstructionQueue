745871cb19231b0f8b4566ae2bdfd484ec05c20a
marclazell <lazell@live.co.uk>
2015-05-21 08:06:53 +0100

    Instruction Message Test and main class

---
+++ src/com/tp/run/InstructionQueueMain.java
@@ -0,0 +1,11 @@
+package com.tp.run;
+
+/**
+ * Created by dev on 21/05/15.
+ */
+public class InstructionQueueMain {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/tp/test/InstructionMessageTest.java
@@ -0,0 +1,36 @@
+package com.tp.test;
+
+/**
+ * Created by dev on 21/05/15.
+ */
+public class InstructionMessageTest {
+
+    // InstructionType 50 - Pass
+    // InstructionType 99 - Pass
+    // InstructionType 1 - Pass
+    // InstructionType 100 - fail
+    // InstructionType 0 - fail
+    // InstructionType -1 - fail
+
+    // ProductCode 5000 pass
+    // ProductCode 0 fail
+    // ProductCode 1 pass
+    // ProductCode -1 fail
+
+    // Quantity 5000 pass
+    // Quantity 0 fail
+    // Quantity 1 pass
+    // Quantity -1 fail
+
+
+    // UOM 0 pass
+    // UOM 1 pass
+    // UOM -1 fail
+    // UOM 255 fail
+
+    // TimeStamp 5000 pass
+    // TimeStamp 0 fail
+    // TimeStamp 1 pass
+    // TimeStamp -1 fail
+
+}
