1a59337154f5aaff9e40c8b0a58e6530c58b98f9
marclazell <lazell@live.co.uk>
2015-05-21 09:33:59 +0100

    Instruction Message Test (unfinished - exceptions not properly handled)

--- src/com/tp/test/InstructionMessageTest.java
+++ src/com/tp/test/InstructionMessageTest.java
@@ -1,36 +1,143 @@
 package com.tp.test;
 
+import com.tp.exception.InvalidMessageException;
+import com.tp.pojo.InstructionMessage;
+import junit.framework.TestCase;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
 /**
  * Created by dev on 21/05/15.
  */
-public class InstructionMessageTest {
+public class InstructionMessageTest extends TestCase {
 
-    // InstructionType 50 - Pass
-    // InstructionType 99 - Pass
-    // InstructionType 1 - Pass
-    // InstructionType 100 - fail
-    // InstructionType 0 - fail
-    // InstructionType -1 - fail
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
 
-    // ProductCode 5000 pass
-    // ProductCode 0 fail
-    // ProductCode 1 pass
-    // ProductCode -1 fail
+    private InstructionMessage im = new InstructionMessage();
 
-    // Quantity 5000 pass
-    // Quantity 0 fail
-    // Quantity 1 pass
-    // Quantity -1 fail
+    private static Integer VALID_INSTRUCTION_TYPE = 75;
+    private static Integer VALID_PRODUCT_CODE = 9999;
+    private static Integer VALID_QUANTITY = 500;
+    private static Integer VALID_UOM = 150;
+    private static Integer VALID_TIMESTAMP = 50;
+
+    private static Integer INVALID_INSTRUCTION_TYPE_LOW = -10;
+    private static Integer INVALID_INSTRUCTION_TYPE_HIGH = 105;
+    private static Integer INVALID_PRODUCT_CODE = -1;
+    private static Integer INVALID_QUANTITY = -1;
+    private static Integer INVALID_UOM_LOW = -1;
+    private static Integer INVALID_UOM_HIGH = 300;
+    private static Integer INVALID_TIMESTAMP = -1;
 
 
-    // UOM 0 pass
-    // UOM 1 pass
-    // UOM -1 fail
-    // UOM 255 fail
+    protected void setUp() throws Exception {
+        super.setUp();
+        im = new InstructionMessage();
+    }
 
-    // TimeStamp 5000 pass
-    // TimeStamp 0 fail
-    // TimeStamp 1 pass
-    // TimeStamp -1 fail
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
 
+    
+
+    public void testInstructionTypeValid() throws InvalidMessageException {
+        instructionMessageValid();
+        instructionTypeInvalidLow();
+        instructionTypeInvalidHigh();
+        productCodeInvalid();
+        quantityInvalid();
+        uomInvalidLow();
+        uomInvalidHigh();
+        timestampInvalid();
+    }
+
+    @Test
+    public void instructionMessageValid() throws InvalidMessageException {       
+        im.setInstructionType(VALID_INSTRUCTION_TYPE);
+        im.setProductCode(VALID_PRODUCT_CODE);
+        im.setQuantity(VALID_QUANTITY);
+        im.setUOM(VALID_UOM);
+        im.setTimeStamp(VALID_TIMESTAMP);
+        assertEquals(im.isInstructionMessageValid(), true);
+    }
+
+    @Test
+    public void instructionTypeInvalidLow() throws InvalidMessageException {
+        exception.expect(InvalidMessageException.class);
+        im.setInstructionType(INVALID_INSTRUCTION_TYPE_LOW);
+        im.setProductCode(VALID_PRODUCT_CODE);
+        im.setQuantity(VALID_QUANTITY);
+        im.setUOM(VALID_UOM);
+        im.setTimeStamp(VALID_TIMESTAMP);
+        im.isInstructionMessageValid();
+    }
+
+    @Test
+    public void instructionTypeInvalidHigh() throws InvalidMessageException {
+        exception.expect(InvalidMessageException.class);
+        im.setInstructionType(INVALID_INSTRUCTION_TYPE_HIGH);
+        im.setProductCode(VALID_PRODUCT_CODE);
+        im.setQuantity(VALID_QUANTITY);
+        im.setUOM(VALID_UOM);
+        im.setTimeStamp(VALID_TIMESTAMP);
+        im.isInstructionMessageValid();
+    }
+
+    @Test
+    public void productCodeInvalid() throws InvalidMessageException {
+        exception.expect(InvalidMessageException.class);
+        im.setInstructionType(VALID_INSTRUCTION_TYPE);
+        im.setProductCode(INVALID_PRODUCT_CODE);
+        im.setQuantity(VALID_QUANTITY);
+        im.setUOM(VALID_UOM);
+        im.setTimeStamp(VALID_TIMESTAMP);
+        im.isInstructionMessageValid();
+    }
+
+    @Test
+    public void quantityInvalid() throws InvalidMessageException {
+        exception.expect(InvalidMessageException.class);
+        im.setInstructionType(VALID_INSTRUCTION_TYPE);
+        im.setProductCode(VALID_PRODUCT_CODE);
+        im.setQuantity(INVALID_QUANTITY);
+        im.setUOM(VALID_UOM);
+        im.setTimeStamp(VALID_TIMESTAMP);
+        im.isInstructionMessageValid();
+    }
+
+    @Test
+    public void uomInvalidLow() throws InvalidMessageException {
+        exception.expect(InvalidMessageException.class);
+        im.setInstructionType(VALID_INSTRUCTION_TYPE);
+        im.setProductCode(VALID_PRODUCT_CODE);
+        im.setQuantity(VALID_QUANTITY);
+        im.setUOM(INVALID_UOM_LOW);
+        im.setTimeStamp(VALID_TIMESTAMP);
+        im.isInstructionMessageValid();
+    }
+
+    @Test
+    public void uomInvalidHigh() throws InvalidMessageException {
+        exception.expect(InvalidMessageException.class);
+        im.setInstructionType(VALID_INSTRUCTION_TYPE);
+        im.setProductCode(VALID_PRODUCT_CODE);
+        im.setQuantity(VALID_QUANTITY);
+        im.setUOM(INVALID_UOM_HIGH);
+        im.setTimeStamp(VALID_TIMESTAMP);
+        im.isInstructionMessageValid();
+    }
+
+    @Test
+    public void timestampInvalid() throws InvalidMessageException {
+        exception.expect(InvalidMessageException.class);
+        im.setInstructionType(VALID_INSTRUCTION_TYPE);
+        im.setProductCode(VALID_PRODUCT_CODE);
+        im.setQuantity(VALID_QUANTITY);
+        im.setUOM(VALID_UOM);
+        im.setTimeStamp(INVALID_TIMESTAMP);
+        im.isInstructionMessageValid();
+    }
 }
