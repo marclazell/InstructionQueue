f681b02c976973e30011bafbb72f50a4ef309448
marclazell <lazell@live.co.uk>
2015-05-21 08:40:01 +0100

    Created InvalidMessageException

---
+++ src/com/tp/exception/InvalidMessageException.java
@@ -0,0 +1,10 @@
+package com.tp.exception;
+
+/**
+ * Created by dev on 21/05/15.
+ */
+public class InvalidMessageException extends Exception {
+    public InvalidMessageException(String exception) {
+        super(exception);
+    }
+}
