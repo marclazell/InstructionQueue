6e71984bfcc478a9c54b4eccda2285fea92df67c
marclazell <lazell@live.co.uk>
2015-05-21 09:03:04 +0100

    Added BooleanUtil class for converting boolean to yes/no

---
+++ src/com/tp/util/BooleanUtil.java
@@ -0,0 +1,15 @@
+package com.tp.util;
+
+/**
+ * Created by dev on 21/05/15.
+ */
+public class BooleanUtil {
+
+    public static String booleanToYesNoConvertor(boolean b) {
+        if (b) {
+            return "Yes";
+        } else {
+            return "No";
+        }
+    }
+}
